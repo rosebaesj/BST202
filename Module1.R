78eadbd70212a0a48abe89e26ff236ad4e4b6c8c
rosebaesj <rosebae@khu.ac.kr>
2022-07-06 17:59:51 -0400

    Module1

--- Module1.R
+++ Module1.R
@@ -1,9 +1,8 @@
 # Module 1
 # Date July 5th 2022
-library(ggplot2) 
 
-
-
+library(ggplot2) #package to plot
+library(haven) #package to import .dta files to R
 
 ##### Step 1: make a csv file from the given table #####
 # the file as spaces so tsv might make errors, use csv just in case
@@ -18,24 +17,9 @@ data <- read.csv("Module1.csv", sep = ",", header = TRUE)
 
 ranked_data <- data[order(data$percap),]
 
-
 ggplot(data, aes(x=percap))+
-  geom_histogram(color="black", fill="grey")+
-  scale_x_continuous(breaks = seq(0,2500,))
-  sjbalabs(x="Dietary trans fat intake (g/day)", y = "Count")+
-  + #16
-  theme(axis.title=element_text(size=50,face = 'bold'),
-        axis.text.x = element_text(size=50),
-        axis.text.y = element_text(size=50),
-        axis.line = element_line(colour = "black"),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.border = element_blank(),
-        panel.background = element_blank(),
-        axis.line.x = element_line(colour = 'black', size=0.75, linetype='solid'),
-        axis.line.y = element_line(colour = 'black', size=0.75, linetype='solid')
-  )
-plot(aofib_ddr_distribution)
+  geom_histogram(color="black", fill="grey", bins = 5)
+
 
 
 
